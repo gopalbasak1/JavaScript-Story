71eb5837623d90f5a6dddc1892362e39922bdc5e
gopalbasak1 <gopalbasak1223@gmail.com>
2024-02-06 12:45:44 +0600

    add one

---
+++ Math/math.js
@@ -0,0 +1,9 @@
+// Math.min()
+// Math.max()
+// Math.abs('difference between the two number');
+// Math.round('2.34 = 2; 2.50 = 3')
+// Math.floor('2.99 = 2; 2.32 = 2')
+// Math.ceil('2.99 = 3; 2.23 = 3');
+// Math.random('always change the numbers randomly');
+
+ console.log(Math.pow(2,3));
